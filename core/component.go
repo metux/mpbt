92b262cbeb9189aa3fbdd24b0408d6ccfa56b133
Enrico Weigelt, metux IT consult <info@metux.net>
2025-10-14 17:48:13 +0200

    1

--- core/component.go
+++ core/component.go
@@ -1,11 +1,5 @@
 package core
 
-import (
-	"log"
-	"os"
-	"strings"
-)
-
 type Component struct {
 	Name     string `yaml:"name"`
 	Provides string `yaml:"provides"`
@@ -19,65 +13,4 @@ func (c *Component) LoadYaml(fn string) error {
 	err := LoadYaml(fn, c)
 	c.Filename = fn
 	return err
-}
-
-type Project struct {
-	Components ComponentMap
-	Provides map[string]ComponentMap
-	Solution Solution
-}
-
-func (db * Project) Add(comp *Component) {
-	if db.Components == nil {
-		db.Components = make(ComponentMap)
-	}
-	db.Components[comp.Name] = comp
-	if db.Provides == nil {
-		db.Provides = make(map[string]ComponentMap)
-	}
-	// FIXME: add support for multiple ones
-	if val, ok := db.Provides[comp.Provides]; ok {
-		// already have it
-		val[comp.Name] = comp
-	} else {
-		newlist := make(ComponentMap)
-		newlist[comp.Name] = comp
-		db.Provides[comp.Provides] = newlist
-	}
-}
-
-func (db * Project) LoadComponents(dirname string) error {
-	entries, err := os.ReadDir(dirname)
-	if err != nil {
-		log.Printf("readdir() error on %s: %s\n", dirname, err)
-		return err
-	}
-
-	for _, entry := range entries {
-		n := entry.Name()
-		if entry.IsDir() {
-			db.LoadComponents(dirname+"/"+n)
-		} else {
-			if strings.HasSuffix(n, ".yaml") || strings.HasSuffix(n, ".yml") {
-				comp := Component{}
-				err = comp.LoadYaml(dirname + "/" + n)
-				if err != nil {
-					log.Printf("load error on %s: %s\n", dirname+"/"+n, err)
-					return err
-				}
-				db.Add(&comp)
-			}
-		}
-	}
-	return nil
-}
-
-func (prj * Project) LoadSolution(fn string) error {
-	return prj.Solution.LoadYaml(fn)
-}
-
-func (prj * Project) Resolve() {
-	for _,b := range prj.Solution.Build {
-		log.Printf("need to build: %s\n", b)
-	}
 }
---
+++ core/project.go
@@ -0,0 +1,67 @@
+package core
+
+import (
+	"log"
+	"os"
+	"strings"
+)
+type Project struct {
+	Components ComponentMap
+	Provides map[string]ComponentMap
+	Solution Solution
+}
+
+func (db * Project) Add(comp *Component) {
+	if db.Components == nil {
+		db.Components = make(ComponentMap)
+	}
+	db.Components[comp.Name] = comp
+	if db.Provides == nil {
+		db.Provides = make(map[string]ComponentMap)
+	}
+	// FIXME: add support for multiple ones
+	if val, ok := db.Provides[comp.Provides]; ok {
+		// already have it
+		val[comp.Name] = comp
+	} else {
+		newlist := make(ComponentMap)
+		newlist[comp.Name] = comp
+		db.Provides[comp.Provides] = newlist
+	}
+}
+
+func (db * Project) LoadComponents(dirname string) error {
+	entries, err := os.ReadDir(dirname)
+	if err != nil {
+		log.Printf("readdir() error on %s: %s\n", dirname, err)
+		return err
+	}
+
+	for _, entry := range entries {
+		n := entry.Name()
+		if entry.IsDir() {
+			db.LoadComponents(dirname+"/"+n)
+		} else {
+			if strings.HasSuffix(n, ".yaml") || strings.HasSuffix(n, ".yml") {
+				comp := Component{}
+				err = comp.LoadYaml(dirname + "/" + n)
+				if err != nil {
+					log.Printf("load error on %s: %s\n", dirname+"/"+n, err)
+					return err
+				}
+				db.Add(&comp)
+			}
+		}
+	}
+	return nil
+}
+
+func (prj * Project) LoadSolution(fn string) error {
+	return prj.Solution.LoadYaml(fn)
+}
+
+func (prj * Project) Resolve() {
+	for _,b := range prj.Solution.Build {
+		log.Printf("need to build: %s\n", b)
+	}
+}
