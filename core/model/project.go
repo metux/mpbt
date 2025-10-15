68e776266fe2c46c2db865c16e9abe7d25cdf030
Enrico Weigelt, metux IT consult <info@metux.net>
2025-10-15 15:27:15 +0200

    1

---
+++ core/model/project.go
@@ -0,0 +1,88 @@
+package model
+
+import (
+	"log"
+	"os"
+	"strings"
+)
+
+type ProvidesMap map[string]ComponentMap
+
+type Project struct {
+	Components ComponentMap
+	Provides ProvidesMap
+	Solution Solution
+}
+
+func (prj * Project) AddComponent(comp *Component) {
+	if prj.Components == nil {
+		prj.Components = make(ComponentMap)
+	}
+	prj.Components[comp.Name] = comp
+	if prj.Provides == nil {
+		prj.Provides = make(map[string]ComponentMap)
+	}
+
+	for _, prov := range comp.Provides {
+		if val, ok := prj.Provides[prov]; ok {
+			// already have it
+			val[comp.Name] = comp
+		} else {
+			newlist := make(ComponentMap)
+			newlist[comp.Name] = comp
+			prj.Provides[prov] = newlist
+		}
+	}
+}
+
+func (prj * Project) LoadComponents(dirname string) error {
+	entries, err := os.ReadDir(dirname)
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range entries {
+		n := entry.Name()
+		if entry.IsDir() {
+			prj.LoadComponents(dirname+"/"+n)
+		} else {
+			if strings.HasSuffix(n, ".yaml") || strings.HasSuffix(n, ".yml") {
+				comp := Component{}
+				if e := comp.LoadYaml(dirname + "/" + n); e != nil {
+					return e
+				}
+				prj.AddComponent(&comp)
+			}
+		}
+	}
+	return nil
+}
+
+func (prj * Project) LoadSolution(fn string) error {
+	return prj.Solution.LoadYaml(fn)
+}
+
+func (prj * Project) LookupComponent(name string) *Component {
+	// apply mapping from solution
+	name = prj.Solution.GetMapped(name)
+
+	// try to find by exact package name
+	if comp, ok := prj.Components[name]; ok {
+		return comp
+	}
+
+	// try by provides
+	if complist, ok := prj.Provides[name]; ok {
+		if (len(complist) == 1) {
+			for _, v := range complist {
+				return v
+			}
+		} else {
+			log.Printf("ERR: multiple results\n");
+			return nil
+		}
+	}
+
+	log.Printf("NOT FOUND %s\n", name)
+	return nil
+}
--- core/project.go
+++ core/project.go
@@ -3,92 +3,12 @@ package core
 import (
 	"fmt"
 	"log"
-	"os"
-	"strings"
 
 	"github.com/metux/mpbt/core/model"
 )
 
-type ProvidesMap map[string]model.ComponentMap
-
 type Project struct {
-	Components model.ComponentMap
-	Provides ProvidesMap
-	Solution model.Solution
-}
-
-func (db * Project) Add(comp *model.Component) {
-	if db.Components == nil {
-		db.Components = make(model.ComponentMap)
-	}
-	db.Components[comp.Name] = comp
-	if db.Provides == nil {
-		db.Provides = make(map[string]model.ComponentMap)
-	}
-
-	for _, prov := range comp.Provides {
-		if val, ok := db.Provides[prov]; ok {
-			// already have it
-			val[comp.Name] = comp
-		} else {
-			newlist := make(model.ComponentMap)
-			newlist[comp.Name] = comp
-			db.Provides[prov] = newlist
-		}
-	}
-}
-
-func (db * Project) LoadComponents(dirname string) error {
-	entries, err := os.ReadDir(dirname)
-	if err != nil {
-		return err
-	}
-
-	for _, entry := range entries {
-		n := entry.Name()
-		if entry.IsDir() {
-			db.LoadComponents(dirname+"/"+n)
-		} else {
-			if strings.HasSuffix(n, ".yaml") || strings.HasSuffix(n, ".yml") {
-				comp := model.Component{}
-				err = comp.LoadYaml(dirname + "/" + n)
-				if err != nil {
-					return err
-				}
-				db.Add(&comp)
-			}
-		}
-	}
-	return nil
-}
-
-func (prj * Project) LoadSolution(fn string) error {
-	return prj.Solution.LoadYaml(fn)
-}
-
-func (prj * Project) LookupComponent(name string) *model.Component {
-	// apply mapping from solution
-	name = prj.Solution.GetMapped(name)
-
-	// try to find by exact package name
-	if comp, ok := prj.Components[name]; ok {
-		return comp
-	}
-
-	// try by provides
-	if complist, ok := prj.Provides[name]; ok {
-		if (len(complist) == 1) {
-			for _, v := range complist {
-				return v
-			}
-		} else {
-			log.Printf("ERR: multiple results\n");
-			return nil
-		}
-	}
-
-	log.Printf("NOT FOUND %s\n", name)
-	return nil
+	model.Project
 }
 
 // resolve what we need to clone
