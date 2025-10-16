39a0ea6ccbfac92ffb368ff8d6067b398000ec11
Enrico Weigelt, metux IT consult <info@metux.net>
2025-10-16 17:00:10 +0200

    1

--- core/workflow/build/builder.go
+++ core/workflow/build/builder.go
@@ -1,4 +1,13 @@
 package build
 
-//type IBuilder interface {
-//}
+import (
+	"github.com/metux/mpbt/core/model"
+)
+
+type IBuilder interface {
+	SetPackage(p * model.Package)
+	RunPrepare() error // eg. autogen.sh
+	RunConfigure() error
+	RunBuild() error
+	RunInstall() error
+}
